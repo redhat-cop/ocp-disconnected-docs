25521ebc262723496dee88ed927c8c0e0818a5c4
Shane Dell <shanedell100@gmail.com>
2024-02-02 15:12:08 -0500

    Update permissions and add missing IAM user.
    - When testing on 4.14 and 4.15 two things were found:
      - The permissions for some of the users and policies were different.
      - There was a user, policy and secret needed for openshift-cloud-network-config-controller-aws.
      - The items for cloud-credential-operator-s3 weren't used but kept it as it didn't hurn anything.
      - Assuming these were updated after 4.13, as 4.13 worked properly without these updates.

--- aws-gov-ipi-dis-maniam/secret-helper.sh
+++ aws-gov-ipi-dis-maniam/secret-helper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# if secrets folder already exists, remove it
+[[ -d secrets ]] && rm -rf secrets || true
 mkdir -p secrets
 for u in $(cat account_names.txt)
 do
@@ -8,4 +10,3 @@ do
   export key=$(jq -r '.AccessKey.SecretAccessKey' <<< $obj)
   envsubst < $(find ./ -name $u*-templ.yaml) > secrets/$u-secret.yaml
 done
- 
